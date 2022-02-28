1b12a71045febcc4229f545b5c218616c33c4d91
CheyNel5 <cnelson9005@gmail.com>
2022-02-28 14:52:42 -0500

    initial commit

---
+++ StaticKeywordExercise/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace StaticKeywordExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var celsius = TempConverter.FahrenheitToCelsius(68);
+            var fahrenheit = TempConverter.CelsiusToFahrenheit(20);
+
+            Console.WriteLine($"Celsius {celsius}");
+            Console.WriteLine($"Fahrenheit {fahrenheit}");
+
+        }
+    }
+}
